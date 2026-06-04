c7e294a1446854725e1a1256e8cd8024795bffe2
Mohammad Nazarkhani <itsnazarkhani@gmail.com>
2026-06-04 14:09:23 +0330

    remove: Removed top bar because in new design we don't need it

--- Client/src/layouts/MainLayout/MainLayout.tsx
+++ Client/src/layouts/MainLayout/MainLayout.tsx
@@ -1,14 +1,10 @@
 import { Outlet } from "react-router-dom";
 import styles from "./MainLayout.module.css";
 import Navbar from "../../components/layout/Navbar/Navbar";
-import TopBar from "../../components/layout/TopBar/TopBar";
 
 function Layout() {
   return (
     <div className={styles.container}>
-      <header className={styles.topbar}>
-        <TopBar />
-      </header>
       <div className={styles.mainContainer}>
         <main className={styles.content}>
           <Outlet />
